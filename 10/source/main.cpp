f55e6925d5aba851a8aa1dcdb7d32e9a7f145f07
Urodish <81559296+kzueirf12345@users.noreply.github.com>
2026-04-25 01:28:14 +0300

    Task 10 (#10)
    * first commit
    
    * simple version
    
    * simd version
    
    * testing
    
    * benches
    
    * BenchPi
    
    * add barriers
    
    * barriers results
    
    * use measurer
    
    * add forgoten berrier destroy
    
    * add report
    
    * update readme
    
    * fix help meassage
    
    * update readme
    
    * fix report
    
    * fix report
    
    * fix report
    
    * fix skip-ahead seed

---
+++ 10/source/main.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <stdexcept>
+
+#include "CliParser/CliParser.hpp"
+#include "Testing/Correctness.hpp"
+#include "Testing/Benchmarking.hpp"
+
+/*!SECTION
+
+===Latency Benchmark===
+std::minstd_rand         Mean: 114.00000  +/- 0.00000 clks
+My MinstdRand            Mean: 114.00000  +/- 0.00000 clks
+My MinstdRandVec         Mean: 7.12500    +/- 0.00000 clks
+
+===Throughput Benchmark===
+std::minstd_rand         Mean: 17.89548   +/- 0.00341 clks
+My MinstdRand            Mean: 8.94688    +/- 0.00137 clks
+My MinstdRandVec         Mean: 2.25551    +/- 0.00057 clks
+
+===Pi Benchmark===
+                 Correct Pi:           3.1415265359
+std::minstd_rand         Pi:           3.1427378800 | Time:      3837439918 clks
+My MinstdRand            Pi:           3.1427378800 | Time:      2103907012 clks
+My MinstdRandVec         Pi:           3.1427378800 | Time:       485354810 clks
+
+// with barriers
+===Pi Benchmark===
+                 Correct Pi:           3.1415265359
+std::minstd_rand         Pi:           3.1415043120 | Time:      2659005388 clks
+My MinstdRand            Pi:           3.1415043120 | Time:      1625531700 clks
+My MinstdRandVec         Pi:           3.1415043120 | Time:       341817068 clks
+
+// with measurer
+===Pi Benchmark===
+std::minstd_rand         Time:       269189134 +/- 389338 clks
+My MinstdRand            Time:       158619265 +/- 197586 clks
+My MinstdRandVec         Time:        34403942 +/- 68539 clks
+
+*/
+
+int main(int argc, char* argv[]) try {
+    CliParser cli(argc, argv);
+    
+    if (cli.shouldExit()) {
+        CliParser::printHelp(std::cout);
+        return EXIT_SUCCESS;
+    }
+
+    const CliParser::Options& opts = cli.options();
+
+    std::ostream* output_ptr = &std::cout;
+    std::ofstream output_file;
+    
+    if (!opts.output_file.empty()) {
+        output_file.open(opts.output_file);
+        if (!output_file.is_open()) {
+            throw std::invalid_argument("Can't open output file: " + opts.output_file);
+        }
+        output_ptr = &output_file;
+    }
+
+    switch (opts.mode) {
+        case CliParser::Mode::TESTING: {
+            minstd_rand::CorrectnessScalar(*output_ptr, opts.seed, opts.iterations_cnt);
+            minstd_rand::CorrectnesVector(*output_ptr, opts.seed, opts.iterations_cnt);
+            break;
+        }
+        case CliParser::Mode::BENCH_LATENCY: {
+            minstd_rand::BenchLatency(*output_ptr, opts.seed, opts.buckets_cnt, opts.iterations_cnt);
+            break;
+        }
+        case CliParser::Mode::BENCH_THROUGHPUT: {
+            minstd_rand::BenchThroughput(
+                *output_ptr, opts.seed, opts.buckets_cnt, opts.batches_cnt, opts.iterations_cnt
+            );
+            break;
+        }
+        case CliParser::Mode::BENCH_PI: {
+            minstd_rand::BenchPi(
+                *output_ptr, opts.seed, opts.buckets_cnt, opts.batches_cnt, opts.iterations_cnt
+            );
+            break;
+        }
+        default: {
+            throw std::invalid_argument("Unkown mode");
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
+catch(const std::exception& e) {
+    std::cerr << "!!!EXCEPTION!!!\n" << e.what() << std::endl;
+    return EXIT_FAILURE;
+}
+catch(...) {
+    std::cerr << "!!!EXCEPTION!!!\n" << "Something went wrong!" << std::endl;
+    return EXIT_FAILURE;
+}
